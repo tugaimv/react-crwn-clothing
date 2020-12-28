83326a600b63f75678cd6dcc6ce5b07b2b153c49
tugaimv <30296401+tugaimv@users.noreply.github.com>
2020-12-28 09:04:35 +0200

    End at redux main reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shoppage.component";
+import Header from "./components/header/header.component";
+import SignInAndSingUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            user: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({
+          user: userAuth,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <>
+        <Header currentUser={this.state.user}></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSingUpPage} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import './sign-in.styles.scss';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+class SignIn extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+      }
+  }
+
+  
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({email: '', password: ''})
+    } catch(err) {
+      console.log(err)
+    }
+    
+  }
+
+  handleChange = event => {
+    const {value, name} = event.target;
+
+    this.setState({ [name]: value })
+  }
+
+  render(){
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput 
+            name='email' 
+            type='email' 
+            value={this.state.email} 
+            handleChange={this.handleChange}
+            label='Email'
+            required 
+          />
+
+          <FormInput 
+            name='password' 
+            type='password' 
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='Password' 
+            required 
+          />
+
+          <div className='buttons'>
+            <CustomButton type='submit'>Sign in</CustomButton>
+            <CustomButton
+              isGoogleSignIn
+              onClick={signInWithGoogle}
+            >
+              Sign in with Google</CustomButton>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn;
