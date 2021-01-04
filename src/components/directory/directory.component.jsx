c59ef788018befe90d72253ea2e5f457e3ff1e54
tugaimv <maksym.tuhai@morion.local>
2021-01-04 17:51:31 +0200

    work at collection page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,58 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import './directory.styles.scss';
 
 import MenuItem from '../menu-item/menu-item.component';
 
-class Directory extends React.Component {
-  constructor(){
-    super();
+const Directory = ({sections}) => (
+  <div className='directory-menu'>
+    {sections.map(({id, ...otherProps}) => (
+      <MenuItem key={id} {...otherProps}/>
+    ))}
+  </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
 
 
-    this.state = {
-      sections: [{
-        title: 'hats',
-        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-        id: 1,
-        linkUrl: 'hats'
-      },
-      {
-        title: 'jackets',
-        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-        id: 2
-      },
-      {
-        title: 'sneakers',
-        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-        id: 3
-      },
-      {
-        title: 'womens',
-        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-        id: 4,
-        size: 'large'
-      },
-      {
-        title: 'mens',
-        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-        id: 5,
-        size: 'large'
-      }
-      ]
-    }
-  }
-
-
-  render(){
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({id, ...otherProps}) => (
-          <MenuItem key={id} {...otherProps}/>
-        ))}
-      </div>
-    )
-  }
-}
-
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
