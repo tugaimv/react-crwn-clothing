1cbc58333886e71fce3f271770314698f609253e
Максим Тугай <maksym.tuhai@morion.local>
2021-01-13 17:48:27 +0200

    using redux thunk to fetch shop page data from redux

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,28 @@
 import ShopActionTypes from './shop.types';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-export const updateCollections = (collections) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
-    payload: collections
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTION_START
 })
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTION_SUCCESS,
+    payload: collectionsMap
+})
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTION_FAILURE,
+    payload: errorMessage
+})
+
+export const fetchCollectionStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections')
+        dispatch(fetchCollectionsStart())
+
+        collectionRef.get().then(snapshot => {
+          const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+          dispatch(fetchCollectionsSuccess(collectionsMap))
+        }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+    }
+}
